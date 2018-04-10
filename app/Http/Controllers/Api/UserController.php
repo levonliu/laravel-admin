828f4a5fd42797146a06a48243f0ff3d94b03451
liuwen <levonliu@outlook.com>
2018-04-10 16:48:22 +0800

    commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Model\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        return '12321312';
+    }
+}
